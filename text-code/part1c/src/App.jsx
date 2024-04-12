28b8ce75b145314707e5e7cc31af6f0f7a4b5fd8
paternostrox <pedro.paternostro@gmail.com>
2024-04-12 20:43:06 -0300

    Text code part1c added

---
+++ text-code/part1c/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button text="plus" onClick={increaseByOne} />
+      <Button text="zero" onClick={setToZero} />
+      <Button text="minus" onClick={decreaseByOne} />
+    </div>
+  )
+}
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
+
+export default App
