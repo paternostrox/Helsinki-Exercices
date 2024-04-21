6f2d960fa583a72eff8a2518041bc96be06fff14
paternostrox <pedro.paternostro@gmail.com>
2024-04-21 20:24:25 -0300

    exercise 1-7 done

--- part1/1-11-unicafe/src/App.jsx
+++ part1/1-11-unicafe/src/App.jsx
@@ -6,19 +6,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  let all = good + neutral + bad;
+  let average = (good - bad) / all;
+  let positivePercentage = (good / all) * 100;
+
   return (
     <div>
       <div id='user-feedback'>
-        <h2>give feedback:</h2>
+        <h2>give feedback</h2>
         <Button text='good' onClick={() => setGood(good + 1)} />
         <Button text='neutral' onClick={() => setNeutral(neutral + 1)} />
         <Button text='bad' onClick={() => setBad(bad + 1)} />
       </div>
       <div id='statistics'>
         <h2>statistics</h2>
-        <Counter text='good' value={good} />
-        <Counter text='neutral' value={neutral} />
-        <Counter text='bad' value={bad} />
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positivePercentage}%</p>
       </div>
     </div>
   )
@@ -29,12 +36,6 @@ const Button = ({text, onClick}) => {
     <button onClick={onClick}>
       {text}
     </button>
-  )
-}
-
-const Counter = ({text, value}) => {
-  return (
-    <p>{text} {value}</p>
   )
 }
 
