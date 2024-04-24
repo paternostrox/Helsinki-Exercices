1d5029ae8dd74cd8ec10f926caefb1afd32e61a8
paternostrox <pedro.paternostro@gmail.com>
2024-04-24 11:16:29 -0300

    part2a text progress

--- text-code/part2a/src/App.jsx
+++ text-code/part2a/src/App.jsx
@@ -1,29 +1,14 @@
-import { useState } from 'react'
-
-const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = newValue => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
+const App = (props) => {
+  const { notes } = props
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li>{note.content}</li>
+        )}
+      </ul>
     </div>
   )
 }
-
-const Display = props => <div>{props.value}</div>
-
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-
-export default App
