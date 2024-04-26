a46ebab0970925ab0dd178283862d3ca5c6d929b
paternostrox <pedro.paternostro@gmail.com>
2024-04-26 14:14:27 -0300

    exercise 2-10 done

--- part2/2-10-phonebook/src/App.jsx
+++ part2/2-10-phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import AddPersonForm from './components/AddPersonForm'
+import SearchPersonField from './components/SearchPersonField'
+import PersonList from './components/PersonList'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -36,22 +39,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddPerson}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <AddPersonForm 
+        handleAddPerson={handleAddPerson} 
+        personName={newName} 
+        setPersonName={setNewName} 
+        personNumber={newNumber}
+        setPersonNumber={setNewNumber}
+      />
       <h2>Numbers</h2>
-      search: <input value={searchString} onChange={(e) => setSearchString(e.target.value)}/>
-      <ul>
-        {filteredPersons.map(person => <li key={person.name}>{person.name} {person.number}</li>)} 
-      </ul>
+      <SearchPersonField 
+        searchString={searchString}
+        setSearchString={setSearchString}
+      />
+      <PersonList persons={persons} />
     </div>
   )
 }
