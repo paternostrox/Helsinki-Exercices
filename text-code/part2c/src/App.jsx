c74b4b664cecc30111963d845b44b6241afcddff
paternostrox <pedro.paternostro@gmail.com>
2024-04-27 20:10:09 -0300

    more progress on part2c text code

--- text-code/part2c/src/App.jsx
+++ text-code/part2c/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
+const port = 3000
+
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [currentNote, setCurrentNote] = useState('hey')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`http://localhost:${port}/notes`)
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const handleAddNote = (event) => {
     event.preventDefault()
--- text-code/part2c/src/main.jsx
+++ text-code/part2c/src/main.jsx
@@ -1,13 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import axios from 'axios'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import App from './App'
-
-const port = 3000
-
-axios.get(`http://localhost:${port}/notes`)
-.then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-})
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
