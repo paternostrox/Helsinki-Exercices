422848a8b7c1ab847377a1adf9cb59ca90e66e24
paternostrox <pedro.paternostro@gmail.com>
2024-05-26 21:56:57 -0300

    eslint stylistic config set

--- text-code/part3/backend/eslint.config.mjs
+++ text-code/part3/backend/eslint.config.mjs
@@ -1,7 +1,33 @@
 import globals from "globals";
+import stylistic from "@stylistic/eslint-plugin-js";
+import js from "@eslint/js";
 
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
+  js.configs.recommended,
+  {
+    plugins: {
+      stylistic
+    },
+    rules: {
+      'stylistic/indent': [
+          'error',
+          2
+      ],
+      'stylistic/linebreak-style': [
+          'error',
+          'unix'
+      ],
+      'stylistic/quotes': [
+          'error',
+          'single'
+      ],
+      'stylistic/semi': [
+          'error',
+          'never'
+      ],
+  }
+}
 ];
