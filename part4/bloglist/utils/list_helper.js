d64e4025b3fc560b00020b9777fb99fdbbc40faa
paternostrox <pedro.paternostro@gmail.com>
2024-11-13 14:05:20 -0300

    exercise 4.4 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, item) => (acc += item.likes), 0)
+}
+
+module.exports = { dummy, totalLikes }
