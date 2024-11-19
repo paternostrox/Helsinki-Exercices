04e9853194d5d9f7b2779913242686aacb6c816a
paternostrox <pedro.paternostro@gmail.com>
2024-11-19 10:54:58 -0300

    backend test is good

--- text-code/part4/backend/mongo.js
+++ text-code/part4/backend/mongo.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
+if (process.argv.length < 3) {
+  console.log("give password as argument")
   process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://pedropaternostro:${password}@cluster0.osbexjo.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://pedropaternostro:${password}@cluster0.osbexjo.mongodb.net/noteAppTest?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false)
 
 mongoose.connect(url)
 
@@ -19,21 +18,21 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema)
 
-// const note = new Note({
-//   content: 'This is not important',
-//   important: false,
-// })
+const note = new Note({
+  content: "HTML is easy",
+  important: true,
+})
 
-// note.save().then(result => {
-//   console.log('note saved!', result)
-//   mongoose.connection.close()
-// })
-
-Note.find({important: true}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+note.save().then((result) => {
+  console.log("note saved!", result)
   mongoose.connection.close()
 })
+
+// Note.find({ important: true }).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
---
+++ text-code/part4/backend/tests/note_api.test.js
@@ -0,0 +1,31 @@
+const { test, after } = require("node:test")
+const assert = require("node:assert")
+const mongoose = require("mongoose")
+const supertest = require("supertest")
+const app = require("../app")
+
+const api = supertest(app)
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/)
+})
+
+test("there are two notes", async () => {
+  const response = await api.get("/api/notes")
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+test("one of the notes is about HTML", async () => {
+  const response = await api.get("/api/notes")
+
+  const contents = response.body.map((e) => e.content)
+  assert(contents.includes("HTML is easy"))
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
