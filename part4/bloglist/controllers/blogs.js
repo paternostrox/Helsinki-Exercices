8862822d3813cf8b826756fc784574df1496375b
paternostrox <pedro.paternostro@gmail.com>
2024-11-23 00:14:09 -0300

    tests cleanup

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -8,8 +8,6 @@ blogsRouter.get("/", (request, response) => {
 })
 
 blogsRouter.post("/", (request, response) => {
-  console.log(request.body)
-
   if (!request.body.title || !request.body.url)
     return response.status(400).end()
 
