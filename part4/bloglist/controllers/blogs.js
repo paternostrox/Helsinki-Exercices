2d623b87b61d731ed49eea9a8420117410a4f06d
paternostrox <pedro.paternostro@gmail.com>
2024-11-27 14:58:53 -0300

    exercise 4.17 done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,30 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
+  response.json(blogs)
 })
 
-blogsRouter.post("/", (request, response) => {
-  if (!request.body.title || !request.body.url)
-    return response.status(400).end()
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body
 
-  const blog = new Blog(request.body)
+  if (!body.title || !body.url) return response.status(400).end()
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
+  const user = await User.findOne({})
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
   })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
