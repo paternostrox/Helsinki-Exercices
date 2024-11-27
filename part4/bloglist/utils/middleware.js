c5446dd4b493e2771676a1e5dc78383d346a658c
paternostrox <pedro.paternostro@gmail.com>
2024-11-27 16:10:35 -0300

    exercise 4.20 done

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,5 +1,16 @@
 const logger = require("./logger")
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "")
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" })
 }
@@ -24,6 +35,7 @@ const errorHandler = (error, request, response, next) => {
 }
 
 module.exports = {
+  tokenExtractor,
   unknownEndpoint,
   errorHandler,
 }
